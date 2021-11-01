db4740f28d9498e87c2a2eed1dd84afdd588aca8
giovanniromanoneto <giovanniromanoneto@gmail.com>
2021-11-01 15:00:57 -0300

    Correção de um bug no login Ajustes no design do login
    #2

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,17 @@
 import React from 'react'
 // import { useHistory } from 'react-router-dom'
-// import { useAuth } from '../auth/useAuth'
+import { useAuth } from '../auth/useAuth'
 
 export function Home () {
   // const history = useHistory()
-  // const { user } = useAuth()
+  const { user } = useAuth()
+
+  console.log(user)
 
   return (
+    <>
         <h1>Home</h1>
+        <h2>{user.email}</h2>
+    </>
   )
 }
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,5 @@
-import { useAuth } from '../auth/useAuth'
 import React, { useState } from 'react'
-import { useHistory, Redirect } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import '../styles/login.css'
 import { firebase } from '../config/firebase'
 
@@ -11,28 +10,28 @@ import Button from 'react-bootstrap/Button'
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import Card from 'react-bootstrap/Card'
+import Alert from 'react-bootstrap/Alert'
+import Image from 'react-bootstrap/Image'
+
+import loja from '../assets/iconLojas.jpg'
 
 export function Inicio () {
   const history = useHistory()
   const [email, setEmail] = useState('')
   const [senha, setSenha] = useState('')
-  const [mensagem, setMensagem] = useState('')
-  const { user } = useAuth()
+  const [sucesso, setSucesso] = useState(true)
 
   async function handleLogin (e) {
     e.preventDefault()
     firebase.auth().signInWithEmailAndPassword(email, senha)
       .then(result => {
-        if (!result) {
-          window.alert('Erro no login!')
-        } else {
-          setMensagem('ok')
-          console.log(mensagem)
+        if (result) {
+          setSucesso(true)
           history.push('/home')
         }
       })
       .catch(err => {
-        setMensagem('erro')
+        setSucesso(false)
         console.log(err)
       })
   }
@@ -41,20 +40,19 @@ export function Inicio () {
     history.push('/cadastroUser')
   }
 
-  if (!user) {
-    return <Redirect to='/home'/>
-  }
-
   return (
-    <Container>
+    <Container className="container">
         <Row id="tela" className="h-100">
-        <Col md={6} className="justify-content center">
-         Imagem
+        <Col md={5} className="colunaMensagem">
+          <h1 className="titulo">Gerenciador de Vendas</h1>
+          <h5 className="subtitulo">Não importa qual seu ramo de atuação, gerencie suas vendas com um sistema descomplicado e intuitivo.</h5>
+          <Image src={loja} fluid/>
+          <p/>
         </Col>
-        <Col className="justify-content center">
-            <Card border="dark" className="justify-content center">
+        <Col md={7} className="colunaLogin">
+            <Card bg="light" style={ { width: '40rem' }}>
                 <Card.Body>
-                    <Card.Title>Faça seu Login</Card.Title>
+                    <Card.Title className="text-center">Faça seu Login</Card.Title>
                         <Form>
                             <Form.Group className="mb-3" controlId="email">
                                 <Form.Label>Email</Form.Label>
@@ -76,12 +74,22 @@ export function Inicio () {
                                 />
                             </Form.Group>
 
-                            <Card.Text>Ainda não se cadastrou? <Card.Link onClick={handleCadastro}>Clique aqui e se cadastre</Card.Link></Card.Text>
+                            <Card.Text className="texto" >Ainda não se cadastrou?</Card.Text>
+                            <Button className="padding-bottom-25px" variant='link' onClick={handleCadastro}>Clique aqui e se cadastre</Button><p/>
 
-                            <Button variant="primary" type="submit" onClick={handleLogin}>
+                            <div className="d-grid gap-2">
+                            <Button variant="primary" type="submit" size="md" onClick={handleLogin}>
                                 Entrar
                             </Button>
+                            </div>
                         </Form>
+                        <p/>
+                        { sucesso
+                          ? (console.log(sucesso))
+                          : (<Alert variant={'danger'} >
+                              Erro no login! Por favor verifique as informações
+                            </Alert>
+                            ) }
                 </Card.Body>
             </Card>
         </Col>
